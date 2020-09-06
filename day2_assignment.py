7211ba32cd959a8a761639bd1d858ae0cca9817d
MohammedHamzaMalik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2020-09-06 09:44:58 +0530

    Add files via upload

---
+++ day2_assignment.py
@@ -0,0 +1,37 @@
+# List and its default functions.
+# Following are the default list functions that Python offers:
+sort()
+append()
+index()
+extend()
+type()
+
+# Dictionary and its default functions.
+# Following are the default dictionary functions that Python offers:
+sorted()
+len()
+any()
+all()
+str()
+
+# Dictionary and its default functions.
+# Following are the default sets functions that Python offers:
+add()
+remove()
+pop()
+discard()
+clear()
+
+# Dictionary and its default functions.
+# Following are the default tuple methods that Python offers:
+index()
+count()
+# There are only two tuple methods count() and index() that a tuple object can call.
+
+# Dictionary and its default functions.
+# Following are the default string methods that Python offers:
+capitalize()
+count()
+endswith()
+find()
+isalpha()
