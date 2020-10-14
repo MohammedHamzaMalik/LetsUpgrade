e8c7a72b345fc05995cbc2d2508e2dbc24926c38
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2020-10-14 15:27:03 +0530

    Update TestEmployee.java

--- Java Essentials/Day 2 Assignment/TestEmployee.java
+++ Java Essentials/Day 2 Assignment/TestEmployee.java
@@ -1,28 +1,29 @@
-class Employee{  
-    int age;  
-    String name;  
-    String city;  
-    void insert(int i, String n, String s) {  
-        age=i;  
-        name=n;  
-        city=s;  
-    }  
+package Assignment1;
+
+class Employee{
+    String name;
+    int age;
+    String city;
+
+    void insert(String n, int a, String c){
+    age = a;
+    name = n;
+    city = c;
+}
     void display(){
-      
-      System.out.println("The name is " + name);
-      System.out.println("The age is " + age);
-      System.out.println("The city is " + city);}  
-}  
-public class TestEmployee {  
-public static void main(String[] args) {  
-    Employee e1=new Employee();  
-    Employee e2=new Employee();  
+        System.out.println("The name is " + name);
+        System.out.println("The age is " + age);
+        System.out.println("The city is " + city);
+    }
+}
 
-    e1.insert(23,"Jack","USA");  
-    e2.insert(21,"Karim","UK");  
-
-    e1.display();  
-    e2.display();  
-
-}  
+public class TestEmployee{
+    public static void main(String[] args){
+        Employee e1 = new Employee();
+        Employee e2 = new Employee();
+        e1.insert( "Amy", 24, "Georgia");
+        e2.insert( "Haris",22, "Waterloo");
+        e1.display();
+        e2.display();
+    }
 }
